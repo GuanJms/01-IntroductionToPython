63fbd9efee5c566f8c2d4d3ba224a1c0dcd8d23c
guans <guans@rose-hulman.edu>
2018-11-27 14:03:17 -0500

    DONE

--- src/m4_SimpleTurtle_objects.py
+++ src/m4_SimpleTurtle_objects.py
@@ -17,6 +17,7 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 # DONE: 1.
 #   Yes, that means for YOU to DO things per the following instructions:
 #
+
 #   On Line 13 above, replace  PUT_YOUR_NAME_HERE  with your OWN name.
 #
 #   BTW, the top block of text above forms what is called a DOC-STRING.
