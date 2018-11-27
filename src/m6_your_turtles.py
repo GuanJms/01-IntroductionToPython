f571cd65c90ceba888bfeccf49e20d788fe1842b
guans <guans@rose-hulman.edu>
2018-11-27 17:53:39 -0500

    DONE

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Shengjun Guan.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,3 +28,37 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+samuel= rg.SimpleTurtle()
+james = rg.SimpleTurtle()
+samuel.pen = rg.Pen('midnight blue', 3)
+samuel.speed = 40
+james.speed = 200
+size = 180
+for k in range(6):
+    samuel.draw_square(size)
+    samuel.pen_up()
+    samuel.right(40)
+    samuel.left(30)
+    samuel.right(30)
+    samuel.backward(40)
+    samuel.pen_down()
+    size = size - 20
+
+james = rg.SimpleTurtle('triangle')
+james.pen = rg.Pen('magenta', 1)
+size2 = 20
+window.tracer(2)
+james.left(130)
+for k in range(6):
+    for g in range(6):
+        james.forward(10)
+        james.draw_square(size)
+    james.backward(30)
+window.close_on_mouse_click()
+
+
+
+
+
